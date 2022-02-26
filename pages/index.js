1eeada23e3847c05f476ef1908a3d69d743fc218
DatSoup <100242733+DatSoup@users.noreply.github.com>
2022-02-26 08:28:32 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Chris Campbell portfolio</a>
+          Welcome to Chris Campbell portfolio</a>
         </h1>
 
       </main>
