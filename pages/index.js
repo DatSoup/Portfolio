27fe22037583fee7c37ca81229e5bd0046a3ac17
DatSoup <100242733+DatSoup@users.noreply.github.com>
2022-02-26 08:40:23 -0600

    dropped anchor

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Chris Campbell portfolio</a>
+          Welcome to Chris Campbell portfolio
         </h1>
 
       </main>
