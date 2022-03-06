5c8f0f28be0bef6a71dbcf5f247d68fb2c32f0a6
DatSoup <100242733+DatSoup@users.noreply.github.com>
2022-03-06 13:42:09 -0600

    Added changes to template

---
+++ components/head.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+export function AppHead(){
+    return(
+        <>
+       
+		<meta charset="UTF-8" />
+        <title>Resume - Chris Campbell</title>
+        <link rel="stylesheet" href="/style/style.css" />
+        <link rel="stylesheet" href="/style/resume.css" />
+        <link rel="stylesheet" media="print" href="/style/print.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&amp;display=swap" rel="stylesheet" />
+        <script src="https://kit.fontawesome.com/fdedd76465.js" crossorigin="anonymous"></script>
+ 
+        </>
+    )
+}
